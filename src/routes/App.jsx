fa96e3ed2b37627159d7ea16f320c7f29f5627ae
dev-rioma <dev.rioma@gmail.com>
2022-02-15 14:00:46 -0500

    changes add media and icons

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -21,12 +21,12 @@ const App = () => {
 					<Route exact path='/' element= {<Home />} />
 					<Route exact path="/login" element = {<Login />}  />
 					<Route exact path="/password-recovery" element= {<PasswordRecovery />} />
-					<Route exact path='/send-email' element={SendEmail} />
-					<Route exact path='/new-password' element={NewPassword} />
-					<Route exact path='/account' element={MyAccount} />
-					<Route exact path='/singup' element={CreateAccount} />
-					<Route exact path='/checkout' element={Checkout} />
-					<Route exact path='/orders' element={Orders} />
+					<Route exact path='/send-email' element={<SendEmail />} />
+					<Route exact path='/new-password' element={<NewPassword />} />
+					<Route exact path='/account' element={<MyAccount />} />
+					<Route exact path='/singup' element={<CreateAccount />} />
+					<Route exact path='/checkout' element={<Checkout />} />
+					<Route exact path='/orders' element={<Orders />} />
 					<Route path='*' element= {<NotFound />} />
 				</Routes>
 			</Layout>
