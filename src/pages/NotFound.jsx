e81d04dfbe2c1daddbd33053983f96e15d15f2b3
dev-rioma <dev.rioma@gmail.com>
2022-02-15 12:33:40 -0500

    first commit

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+
+const NotFound = () => {
+    return (
+
+        <div className="section">
+            <h1>Not Found, Error 404.</h1>
+        </div>
+    );
+}
+
+export default NotFound;
