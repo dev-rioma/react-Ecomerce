1eedc4e6f130020a85620da41b62e2ad1ef626af
dev-rioma <dev.rioma@gmail.com>
2022-02-17 17:47:07 -0500

    Add OrderCart

--- src/containers/MyOrder.jsx
+++ src/containers/MyOrder.jsx
@@ -1,30 +1,33 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import OrderItem from '@components/OrderItem';
+import AppContext from '../context/AppContext';
 import '@styles/MyOrder.scss';
-
-import flechita from '@icons/flechita.svg';
+import arrow from '@icons/flechita.svg';
 
 const MyOrder = () => {
-    return (
-        <aside className="product-detail">
-            <div className="title-container">
-            <img src={flechita} alt="arrow" />
-            <p className="title">My order</p>
-            </div>
-            <div className="my-order-content">
-                <OrderItem />
-                <div className="order">
-                <p>
-                    <span>Total</span>
-                </p>
-                <p>$560.00</p>
-                </div>
-                <button className="primary-button">
-                    Checkout
-                </button>
-            </div>
-        </aside>
-    );
+	const { state } = useContext(AppContext);
+	return (
+		<aside className="MyOrder">
+			<div className="title-container">
+				<img src={arrow} alt="arrow" />
+				<p className="title">My order</p>
+			</div>
+			<div className="my-order-content">
+				{state.cart.map(product => (
+					<OrderItem product={product} key={`orderItem-${product.id}`} />
+				))}
+				<div className="order">
+					<p>
+						<span>Total</span>
+					</p>
+					<p>$560.00</p>
+				</div>
+				<button className="primary-button">
+					Checkout
+				</button>
+			</div>
+		</aside>
+	);
 }
 
 export default MyOrder;
